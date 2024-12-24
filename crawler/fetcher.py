b4afeab95aa4398c7dcec4a3f1c0f56843e74f47
wd40wd40wd40 <lgyulgyu@uci.edu>
2024-12-23 20:05:00 -0800

    Redid fetcher.py

--- crawler/fetcher.py
+++ crawler/fetcher.py
@@ -1,26 +1,27 @@
 import aiohttp
 import logging
 
-logging.basicConfig(level=logging.INFO)
 
-" Handles HTTP requests and manages connections using aiohttp. "
-class Fetcher:  
+#Connect to HTML and manage https connections using aiohttp
+class Fetcher:
+    # instantiate
+    def __init__(self, timeout = 10):
+        self.timeout = aiohttp.ClientTimeout(total=timeout) # using a format aiohttp is expecting
 
-    def __init__(self, timeout=10):
-        "Initializes the fetcher with a default timeout."
-        self.timeout = aiohttp.ClientTimeout(total=timeout)
-
+    # create the manager to handle connection pooling
     async def create_session(self):
-        " Creates an aiohttp session with custom configurations. "
-        return aiohttp.ClientSession(timeout=self.timeout) # Manager to handle mutliple https requests
-
-    async def fetch(self, session, url):
-        " Fetches the content of a URL. "
+        return aiohttp.ClientSession(timeout = self.timeout)
+    
+    # Get the content of the url 
+    async def fetch(self, session, url): # reuse the session manager to save on efficiency with 'session' as parameter (not opening and closing inside fetch)
         try:
-            async with session.get(url, allow_redirects=True) as response:
+            async with session.get(url, allow_redirects = True) as response: #gives instance of session
+                #if succesful connection and is website 
                 if response.status == 200 and "text/html" in response.headers.get("Content-Type", ""):
                     return await response.text()
-                logging.warning(f"Skipping non-HTML or failed URL: {url}")
+                logging.warning(f"Skipping non-html or failed URL: {url}")
+
         except Exception as e:
             logging.error(f"Error fetching {url}: {e}")
         return None
+    
