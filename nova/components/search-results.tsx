1adf446bf5211042a532fd526360491bf10701ab
Aram Petrosyan <142950097+aarampetrosyann@users.noreply.github.com>
2025-01-09 16:46:57 -0800

    added hover and visited effect to search results

--- nova/components/search-results.tsx
+++ nova/components/search-results.tsx
@@ -72,8 +72,8 @@ export function SearchResults({ query }: SearchResultsProps) {
         {results.map((res, index) => (
           <div key={index} className='max-w-2xl'>
             {/* TESTING */}
-            <h2 className='text-lg font-medium text-primary mb-1'>
-              <Link href={res.doc_id}>{res.title}</Link>
+            <h2 className='text-lg font-medium text-primary mb-1 hover:underline'>
+              <Link href={res.doc_id} className='visited:text-violet-900' target='_blank'>{res.title}</Link>
             </h2>
             {/* TESTING */}
             <h4 className='text-lg font-medium text-primary mb-1'>
